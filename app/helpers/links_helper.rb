106d87590976849a0001536b86ce969b415ad2f2
Anett Gesierich <anett@emsuiko.de>
2015-07-11 20:52:19 +0200

    add url validation and messages

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,5 @@
 module LinksHelper
+  def confirm_link_deletion(link)
+    "#{t('messages.delete.link', name: link.name)}"
+  end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
 class Link < ActiveRecord::Base
   belongs_to :user
+  validates :url, presence: true
+  validates :url, format: { with: URI.regexp }
 end
