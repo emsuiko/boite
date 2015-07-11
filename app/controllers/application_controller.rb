267113caf9fc2fde457c1fef05295459481ee1ab
Anett Gesierich <anett@emsuiko.de>
2015-07-11 20:55:13 +0200

    remove unused method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def switch_language
-    if I18n.locale == :de
-      I18n.locale = :en
-    else
-      I18n.locale = :de
-    end 
-
-    redirect_to :back
-  end
 end
