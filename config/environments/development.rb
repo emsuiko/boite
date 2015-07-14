964426f7b8b8b2d9693c860e5a4aa197171f9a13
Anett Gesierich <anett@emsuiko.de>
2015-07-14 15:16:54 +0200

    setup mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,9 +14,22 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   # devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.emsuiko.de",
+    port: 587,
+    domain: ENV["MAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["MAIL_USER"],
+    password: ENV["MAIL_PWD"],
+    openssl_verify_mode: 'none'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
